f8bf76b74ed053654003be1491d38a005074c530
offyerrocker <offyerrocker88@gmail.com>
2025-12-22 21:32:00 -0800

    update mm key
    update mm key for epic games matchmaking

---
+++ lua/networkmatchmakingepic.lua
@@ -0,0 +1,7 @@
+--this file is responsible for making sure that only lobbies with this mod installed are compatible
+
+local version = 4
+
+_G.CLIENTSIDE_DETECTION_ORIG_MM_KEY = _G.CLIENTSIDE_DETECTION_ORIG_MM_KEY or NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY
+
+NetworkMatchMakingEPIC._BUILD_SEARCH_INTEREST_KEY = _G.CLIENTSIDE_DETECTION_ORIG_MM_KEY ..  "_hoxi_clientside_detection_r" .. tostring(version)
